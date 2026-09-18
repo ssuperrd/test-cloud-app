7d76712d3f2349080fe21ab847cefe73f3144be7
denvergargar-wtw <denver.gargar@wtwco.com>
2026-09-19 07:06:19 +0800

    Clean baseline code

--- app-ui/src/app/app.ts
+++ app-ui/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Estetiks Millenials');
+  protected readonly title = signal('Aesthetic Clinic');
 }
