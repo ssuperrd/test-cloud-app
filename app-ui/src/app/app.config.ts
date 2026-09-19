4bea3865950a2e6de407e4ce51be527598d6bf8a
denvergargar-wtw <denver.gargar@wtwco.com>
2026-09-19 08:20:56 +0800

    update app.config.ts and angular.json

--- app-ui/src/app/app.config.ts
+++ app-ui/src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { authTokenInterceptor } from './core/http/auth-token.interceptor';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authTokenInterceptor])),
-    provideClientHydration(),
   ],
 };
